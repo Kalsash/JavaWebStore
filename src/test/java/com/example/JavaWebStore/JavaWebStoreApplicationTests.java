50b2a31d317b1e26a93ff2afce50414cf8783a67
Александр Калинин <kalsash@yandex.ru>
2023-05-09 17:42:33 +0300

    Initial

---
+++ src/test/java/com/example/JavaWebStore/JavaWebStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaWebStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
